0d887be2ba2007f57acf56f93c77cb70ca01ea28
rdjrjqouqcu <rdjrjqouqcu@gmail.com>
2023-03-11 22:03:57 -0800

    Most of solid fuel complete

--- rd-fuelmix/data.lua
+++ rd-fuelmix/data.lua
@@ -1,87 +1,140 @@
 
-local entity = {
-  -- PrototypeBase
-  name = "waterbox",
-  type = "simple-entity-with-owner",
-  -- Prototype/Entity
-  icon = "__rd-waterbox__/graphics/waterbox.png",
-  icon_size = 40,
-  allow_copy_paste = true,
-  collision_box = {{-0.49, -0.49}, {0.49, 0.49}},
-  flags = {"player-creation"},
-  minable = { mining_time = 1, result = "waterbox", count = 1, mining_particle = "wooden-particle"},
-  placeable_by = {item = "waterbox", count = 1},
-  remove_decoratives = true,
-  selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-  -- Prototype/EntityWithHealth
-  alert_when_damaged = false,
-  corpse = nil,
-  create_ghost_on_death = false,
-  dying_explosion = table.deepcopy(data.raw["container"]["wooden-chest"]["dying_explosion"]),
-  max_health = 10,
-  -- Prototype/SimpleEntityWithOwner
-  picture = {
-    filename = "__rd-waterbox__/graphics/waterbox.png",
-    size = 40,
-  },
+
+local recipe_category_fuel_processing = {
+  type = "recipe-category",
+  name = "fuel-processing"
 }
 
-local item = {
-  -- PrototypeBase
-  name = "waterbox",
+-- data:extend()
+
+--
+--  SOLID MIXER
+--
+
+local include_solid_mixer = settings.startup["rd-fuelmix-solid-enable"].value
+
+local item_solid_fuel_mix = {
   type = "item",
-  -- Prototype/Item
-  icon = "__rd-waterbox__/graphics/waterbox.png",
-  icon_size = 40,
+  name = "fuelmix-solid",
+  icon = "__rd-fuelmix__/graphics/fuel-solid.png",
+  icon_size = 512,
+  icon_mipmaps = 1,
+  fuel_value = settings.startup["rd-fuelmix-solid-fuelvalue"].value .. "MJ",
+  subgroup = "raw-material",
+  stack_size = 100,
+  fuel_category = "chemical",
+  fuel_acceleration_multiplier = settings.startup["rd-fuelmix-solid-acceleration"].value,
+  fuel_top_speed_multiplier = settings.startup["rd-fuelmix-solid-topspeed"].value,
+}
+
+local item_solid_fuel_mixer = {
+  type = "item",
+  name = "fuelmixer-solid",
+  icon = "__rd-fuelmix__/graphics/mixer-solid.png",
+  icon_size = 512,
+  icon_mipmaps = 1,
+  subgroup = "smelting-machine",
+  order = "z",
+  place_result = "fuelmixer-solid",
   stack_size = 50,
-  place_result = "waterbox",
 }
 
-local chest_item = "wooden-chest"
-if settings.startup["rd-waterbox-iron-chest"].value  then
-  chest_item = "iron-chest"
-end
-
-local recipe = {
-  -- PrototypeBase
-	type = "recipe",
-  name = "waterbox",
-	order = "z",
-  -- Prototype/Recipe
-	ingredients = {
-		{"explosives", 10},
-		{chest_item, 1},
-		{"grenade", 1},
-	},
-	category = "crafting",
-	enabled = "false",
-	energy_required = 4,
-	result = "waterbox",
-	subgroup = "terrain",
+local recipe_solid_mixer = {
+  type = "recipe",
+  name = "fuelmixer-solid",
+  enabled = false,
+  energy_required = 4,
+  ingredients = {
+    {"iron-plate", 10},
+    {"stone-brick", 10},
+  },
+  result = "fuelmixer-solid",
 }
 
-local research = {
+local recipe_solid_mix = {
+  type = "recipe",
+  name = "fuel-processing-solid",
+  enabled = false,
+  energy_required = 1,
+  ingredients = {},
+  category = "fuel-processing",
+  result = "fuelmix-solid",
+}
+
+local entity_solid_mixer = {
   -- PrototypeBase
-  type = "technology",
-  name = "waterboxes",
-  -- Prototype/Technology
-  icon = "__rd-waterbox__/graphics/waterbox-technology.png",
-  icon_size = 100,
-  unit = data.raw["technology"]["cliff-explosives"]["unit"],
-  effects = {
-    {
-      type = "unlock-recipe",
-      recipe = "waterbox"
+  name = "fuelmixer-solid",
+  type = "assembling-machine",
+
+  -- Prototype/AssemblingMachine
+  fixed_recipe = "fuel-processing-solid",
+
+  -- Prototype/CraftingMachine
+  crafting_categories = { "fuel-processing" },
+  crafting_speed = 1,
+  energy_source = {
+    type = "burner",
+    emissions_per_minute = 4,
+    fuel_inventory_size = 4,
+    fuel_category = "chemical",
+    light_flicker = {
+      minimum_light_size = 1,
+      light_intensity_to_size_coefficient = 0.2,
+      color = {1,0.6,0},
+      minimum_intensity = 0.05,
+      maximum_intensity = 0.2
+    },
+  },
+  energy_usage = settings.startup["rd-fuelmix-solid-fuelrequired"].value .. "MW",
+  allowed_effects = { "pollution"},
+  animation = {
+    layers = {
+      {
+        filename = "__rd-fuelmix__/graphics/mixer-solid.png",
+        frame_count = 1,
+        width = 512,
+        height = 512,
+        -- 3 tiles = 96 pixels
+        scale = (96 - 6) / 512,
+      },
     }
   },
-  prerequisites = data.raw["technology"]["cliff-explosives"]["prerequisites"],
+
+  -- Prototype/EntityWithOwner
+
+  -- Prototype/EntityWithHealth
+  max_health = 150,
+  corpse = "big-remnants",
+  dying_explosion = "medium-explosion",
+
+  -- Prototype/Entity
+  icon = "__rd-fuelmix__/graphics/mixer-solid.png",
+  icon_size = 40,
+  minable = { mining_time = 0.2, result = "fuelmixer-solid" },
+  placeable_by = {item = "fuelmixer-solid", count = 1},
+  collision_box = { { -1.4, -1.4, }, { 1.4, 1.4 } },
+  drawing_box = {{-1.5, -1.5}, {1.5, 1.5}},
+  flags = {
+    "placeable-neutral",
+    "placeable-player",
+    "player-creation",
+  },
+  selection_box = { { -1.5, -1.5 }, { 1.5, 1.5 } },
+  working_sound = {
+    sound = {
+      filename = "__base__/sound/burner-mining-drill.ogg",
+      volume = 0.8
+    }
+  },
 }
 
-local separate_research = settings.startup["rd-waterbox-separate-research"].value
-
-data:extend{entity, item, recipe}
-if separate_research then
-  data:extend{research}
-else
-    table.insert(data.raw["technology"]["cliff-explosives"].effects, {type = "unlock-recipe",recipe = "waterbox"})
+if include_solid_mixer then
+  data:extend{
+    item_solid_fuel_mix,
+    item_solid_fuel_mixer,
+    recipe_solid_mixer,
+    recipe_solid_mix,
+    entity_solid_mixer,
+    recipe_category_fuel_processing
+  }
 end
--- rd-fuelmix/settings.lua
+++ rd-fuelmix/settings.lua
@@ -1,16 +1,37 @@
 data:extend({
 	{
 		type = "bool-setting",
-		name = "rd-waterbox-separate-research",
+		name = "rd-fuelmix-solid-enable",
 		setting_type = "startup",
 		default_value = true,
 		order = "a",
 	},
 	{
-		type = "bool-setting",
-		name = "rd-waterbox-iron-chest",
+		type = "int-setting",
+		name = "rd-fuelmix-solid-fuelrequired",
 		setting_type = "startup",
-		default_value = false,
+		default_value = 10,
 		order = "b",
+	},
+	{
+		type = "int-setting",
+		name = "rd-fuelmix-solid-fuelvalue",
+		setting_type = "startup",
+		default_value = 10,
+		order = "b",
+	},
+	{
+		type = "double-setting",
+		name = "rd-fuelmix-solid-acceleration",
+		setting_type = "startup",
+		default_value = 1.0,
+		order = "c",
+	},
+	{
+		type = "double-setting",
+		name = "rd-fuelmix-solid-topspeed",
+		setting_type = "startup",
+		default_value = 1.0,
+		order = "d",
 	},
 })
